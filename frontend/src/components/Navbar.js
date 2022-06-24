4a1e07c3e3fb4ea1bb5da716bca92b6da6a39a8f
Itza Zujuy Blanco <itza_zujuy@hotmail.co.uk>
2022-06-23 21:37:16 -0500

    borramos opciones de navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,12 +9,6 @@ const Navbar = () => (
       <li>
         <Link to="/">Home</Link>
       </li>
-      <li>
-        <Link to="/signup">Signup</Link>
-      </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
     </ul>
   </StyledNavbar>
 );
